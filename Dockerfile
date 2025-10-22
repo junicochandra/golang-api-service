aaf03cbd23d11e4596d4e1246c89a0352053f65a
Junico Dwi Chandra <junico.chandra@playonsports.com>
2025-10-22 16:38:55 +0700

    setup swagger

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
 # Use the official lightweight Go image based on Alpine Linux
 FROM golang:1.24-alpine
 
+# Install necessary tools
+RUN apk add --no-cache git
+
+# Install swag CLI globally
+RUN go install github.com/swaggo/swag/cmd/swag@latest
+
 # Set the working directory inside the container
 WORKDIR /app
 
@@ -10,8 +16,14 @@ COPY . .
 # Download Go module dependencies (like go.mod and go.sum)
 RUN go mod tidy
 
+# Generate Swagger
+RUN swag init
+
 # Expose port 9000 so it can be accessed from outside the container
 EXPOSE 9000
+
+# Add Go bin to PATH so swag is accessible
+ENV PATH="/go/bin:${PATH}"
 
 # Run the application directly from source code (no build needed)
 CMD ["go", "run", "main.go"]
--- main.go
+++ main.go
@@ -1,18 +1,54 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
+	echoSwagger "github.com/swaggo/echo-swagger"
+
+	_ "github.com/junicochandra/golang-api-service/docs"
 )
 
+// @title Golang API Service
+// @version 1.0
+// @description This is a RESTful API service built with Golang for managing data and handling requests efficiently.
+// @contact.name Junico Dwi Chandra
+// @contact.url https://junicochandra.com/
+// @contact.email junicodwi.chandra@gmail.com
+// @host localhost:9000
+// @BasePath /api/v1
 func main() {
-	fmt.Println("Starting server on :9000")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Hello world!")
-	})
+	e := echo.New()
 
-	err := http.ListenAndServe(":9000", nil)
-	if err != nil {
-		return
+	g := e.Group("/api/v1")
+
+	g.POST("get-product", GetProduct)
+
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
+
+	e.Logger.Fatal(e.Start(":9000"))
+}
+
+type ProductRequest struct {
+	ID    int    `json:"id" example:"1"`
+	Name  string `json:"name" example:"Sample Product"`
+	Price int    `json:"price" example:"100"`
+}
+
+// @Tags Product
+// @Summary Get Product
+// @Description Get list of products
+// @Router /get-product [post]
+// @Param request body ProductRequest true "Payload Body [RAW]"
+// @Accept json
+// @Produce json
+// @Success 200 {array} ProductRequest
+// @Failure 400
+func GetProduct(c echo.Context) error {
+	data := ProductRequest{
+		ID:    2,
+		Name:  "Sample Product 2",
+		Price: 100,
 	}
+	return c.JSON(http.StatusOK, data)
 }
